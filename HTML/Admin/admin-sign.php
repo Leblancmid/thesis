95b5bc48fe3279eadbeffba41657798a6bed22e9
Ken Caburnay <gilbertcaburnay@gmail.com>
2024-04-28 14:01:27 +0800

    rename login form

--- HTML/Admin/admin-sign.php
+++ HTML/Admin/admin-sign.php
@@ -12,7 +12,7 @@ session_start();
    <meta charset="UTF-8">
    <meta http-equiv="X-UA-Compatible" content="IE=edge">
    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-   <title>login form</title>
+   <title>Login Form</title>
    <link rel="stylesheet" href="../../CSS/sign-style.css">
 
 </head>
